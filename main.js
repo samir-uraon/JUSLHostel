fe719c1008e866722aa000a88888134b33fe5b1e
Samir Uraon <pclenovo780@gmail.com>
2024-12-29 15:30:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,380 @@
+var data = [
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Samir Uraon',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-25',
+    Home: 'Alipurduar'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Rakesh Sahu',
+    Department: 'Power Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-20',
+    Home: 'Alipurduar'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Amrit Das',
+    Department: 'Construction Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-11',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Riju Paul',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-21',
+    Home: 'Murshidabad'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Sohal Rana',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-22',
+    Home: 'Malda'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'MD Nasim Mondal',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-09',
+    Home: 'Bankura'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Rahul Bera',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-07',
+    Home: 'Jhargram'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'MD Soyab',
+    Department: 'Power Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-26',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Nahid Aktar',
+    Department: 'Power Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-24',
+    Home: 'Malda'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Arpan Kumar Dandapat',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-18',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Monidip Das',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-16',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Jayad Hunssari',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-17',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Soumen Chouwdary',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-23',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Zubar Ahamad',
+    Department: 'Power Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-27',
+    Home: 'Kashmir'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Khakan Barman',
+    Department: 'Power Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-13',
+    Home: 'Cochbihar'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Azad Mondal',
+    Department: 'Information Technology',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-12',
+    Home: 'Bhardhaman'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Sk MD Fayaz',
+    Department: 'Instrumentation And Electronics Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-08',
+    Home: 'Digha'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Abijit Kumar Paul',
+    Department: 'Construction Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-19',
+    Home: 'North Dinajpur'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Sakil Ahamad',
+    Department: 'Construction Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-15',
+    Home: 'Jhargram'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Sk Mustakin',
+    Department: 'Instumentation And Electronics Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-14',
+    Home: 'None'
+  },
+  {
+    pic: 'https://th.bing.com/th/id/R.92ecae7a52e69f4e380760d06b4791dc?rik=6LEdIyVUjit%2byA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-NfNW3SUM2XU%2fUOybexBe3SI%2fAAAAAAAAApQ%2fLRQX9HK-NpA%2fs1600%2funknown.jpg&ehk=k8DzY9YXoaLCwLHfmT7%2fA7ArmB2SCXZIBV601NI5%2bBk%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+    Name: 'Orgho Boiydo',
+    Department: 'Instumentation And Electronics Engineering',
+    Year: '1st',
+    University: 'Jadavpur University',
+    HostelRoomNo: 'S1-10',
+    Home: 'Jhargram'
+  }
+]
+
+function topposition() {
+  window.scrollTo(0, 0, "smooth")
+}
+
+window.addEventListener("load", () => {
+  starting()
+})
+
+// function pressenteroff(evt){
+//   let bool=true
+//   if(evt.keyCode==13){
+//    bool=false
+//   }  
+//   return bool
+// }
+
+
+
+
+function test(){
+  return false
+}
+// ^
+// this two is same in technically(if you use onsubmit="return test()" in form tag)
+// |
+// document.querySelector("form").addEventListener("submit",(event)=>{
+//   event.preventDefault()
+// })
+
+
+
+
+url = "https://samir-uraon.github.io/hostapi/"
+
+let starting = async () => {
+
+  // let response=await fetch(url)
+  // let data2=await response.json()
+  // let prefactdata=[...new Set(data2)]
+  
+
+  let perfactdata = [...new Set(data)]
+  displaydata(perfactdata)
+
+  let options = Object.keys(perfactdata[0])
+  let select = document.querySelector("select")
+
+  options.map((option) => {
+    
+    if (option != "pic") {  
+      let newele = document.createElement("option")
+      newele.innerHTML = option
+      newele.style.textTransform = "capitalize"
+      newele.value = option 
+      if (option == "Name") {
+        newele.selected = "selected"
+      }
+      select.append(newele)
+    }
+  })
+
+
+}
+
+
+function displaydata(a) {
+  document.querySelector(".containers").innerHTML = a.map((value, index) => {
+    // let results = a[index]
+    let results=value
+    return (`<div class="search_data">
+            <div class="image_div">
+             <img src="https://plus.unsplash.com/premium_photo-1681488240099-f1f8585ef3e5?q=80&w=2038&auto=format&fit=crop&ixlib=rb-4.
+            0.3&ixid=M3wxMjA3fDB
+            8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+           </div>
+            <div class="details">
+             <p><span style="font-weight: bold;">Name : </span>${results.Name}</p>
+             <p><span style="font-weight: bold;">Department : </span>${results.Department}</p>
+             <p><span style="font-weight: bold;">Year : </span>${results.Year}</p>
+             <p><span style="font-weight: bold;">University : </span>${results.University}</p>
+             <p><span style="font-weight: bold;">HostelRoomNo : </span>${results.HostelRoomNo}</p>
+             <p><span style="font-weight: bold;">Home : </span>${results.Home}</p>
+            </div>
+           <a href="https://unsplash.com/photos/golden-cylinder-with-glass-spheres-and-christmas-ornaments-around-it-for-product-display-3d-rendering-K0pdfzPHAsI" target="_blank">More Details</a>
+  </div>`)
+  }).join(" ")
+}
+
+
+// function search(d) {
+//   let searchdata = d
+//   let searchby=document.querySelector("select").value
+
+//   if(document.querySelector("input").value!=""){
+
+//   let searchlist=document.querySelectorAll(".search_data .details p")
+//   searchlist.forEach((value)=>{
+
+//     if(value.children[0].textContent.includes(searchby)){
+    
+//       if(value.childNodes[1].textContent.toLowerCase().includes(searchdata)){   
+        
+//         value.parentElement.parentElement.style.display="flex"
+//       }
+//       else{
+        
+//           value.parentElement.parentElement.style.display="none"
+//         }
+//     }
+//   })
+
+// }else{
+  
+//   let det=document.querySelector(".containers").children
+// for (const element of det) {
+//    element.style.display="flex"
+// }        
+//     }
+// }
+
+
+
+// document.querySelector("select").addEventListener("change",()=>{
+//   let det=document.querySelector(".containers").children
+// for (const element of det) {
+//    element.style.display="flex"
+// }
+// search(document.querySelector("input").value)
+// })
+
+// document.querySelector("input").addEventListener("keyup", (e) => {
+//   let sd = e.target.value.toLowerCase()
+//   search(sd)
+// })
+
+
+
+
+
+
+
+async function search2(d2){
+  if(document.querySelector("input").value!=""){
+    let searchby=document.querySelector("select").value
+   
+    // let response=await fetch(url)
+  // let data2=await response.json()
+  // let perfactdata=[...new Set(data2)]
+
+    let perfactdata = [...new Set(data)]
+  let filterdata=perfactdata.filter((value)=>{
+    
+    return value[searchby].toLowerCase().includes(d2)
+  })
+displaydata(filterdata)
+
+  }else{
+// let response=await fetch(url)
+  // let data2=await response.json()
+  // let prefactdata=[...new Set(data2)]
+
+  let perfactdata = [...new Set(data)]
+  displaydata(perfactdata)
+  }
+}
+
+
+document.querySelector("select").addEventListener("change",()=>{
+// let response=await fetch(url)
+  // let data2=await response.json()
+  // let perfactdata=[...new Set(data2)]
+
+  let perfactdata = [...new Set(data)]
+  displaydata(perfactdata)
+
+search2(document.querySelector("input").value)
+})
+
+
+
+document.querySelector("input").addEventListener("keyup", (e) => {
+  let sd = e.target.value.toLowerCase()
+  search2(sd)
+})
+
+
+
+ 
